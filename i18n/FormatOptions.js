db6ec2e33d64f5dce8d8726bbe7b1ebfe1762aaa
felixlinker <linkerfelix@gmail.com>
2018-09-26 20:09:52 +0100

    Add first part of internationalization

---
+++ i18n/FormatOptions.js
@@ -0,0 +1,13 @@
+
+/**
+ * @typedef {Object} FormattingObject
+ * @property {string} format
+ * @property {string} unit
+ * @property {string} [storedUnit]
+ */
+
+ /**
+  * Has various properties of type {@link FormattingObject} that define how
+  * specific module/ship properties are to be formatted.
+  */
+export const PROPERTY_FORMATTING = {};
