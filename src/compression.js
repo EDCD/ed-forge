0d6763f776bc2f3c0de6ce595f5594148244d90f
felixlinker <linkerfelix@gmail.com>
2018-11-11 01:22:39 +0000

    add (de)compression

--- src/compression.js
+++ src/compression.js
@@ -1,12 +1,21 @@
+import pako from 'pako';
 
 /**
  * @param {Object} json
  * @return {string}
  */
-export function compress(json) {}
+export function compress(json) {
+  const string = JSON.stringify(json);
+  const deflated = pako.deflate(string, { to: 'string' });
+  return encodeURIComponent(Buffer.from(deflated).toString('base64'));
+}
 
 /**
  * @param {string} compressedBuild
  * @return {object}
  */
-export function decompress(compressedBuild) {}
+export function decompress(compressedBuild) {
+  const decoded = Buffer.from(decodeURIComponent(compressedBuild), 'base64');
+  const inflated = pako.inflate(decoded, { to: 'string' });
+  return JSON.parse(inflated);
+}
