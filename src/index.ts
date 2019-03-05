6e55fdf262bcbeac894b769793f2e5f84a170651
Felix Linker <linkerfelix@gmail.com>
2019-03-06 00:33:45 +0100

    Export ship statistics

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,11 @@
 import Ship from './Ship';
 import Module from './Module';
 import Factory from './data';
+import * as ShipProps from './ship-stats';
 
 export {
     Ship,
     Module,
     Factory,
+    ShipProps,
 };
