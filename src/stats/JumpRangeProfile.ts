0cf9d331ad6727362c3573b5ce0d6ad163f3fd55
Felix Linker <linkerfelix@gmail.com>
2020-11-01 03:08:50 +0100

    Add isolated jump-range calculation function

--- src/stats/JumpRangeProfile.ts
+++ src/stats/JumpRangeProfile.ts
@@ -26,6 +26,24 @@ export interface IJumpRangeMetrics {
     jumpBoost: number;
 }
 
+export function calculateJumpRange(
+    ship: Ship,
+    mass: number,
+    fuelUsed: number,
+    modified: boolean,
+): number {
+    const fsd = ship.getFSD();
+    const optMass = fsd.getClean('fsdoptimalmass', modified);
+    const maxFuelPerJump = fsd.getClean('maxfuel', modified);
+    const fuelMul = fsd.getClean('fuelmul', modified);
+    const fuelPower = fsd.getClean('fuelpower', modified);
+
+    return (Math.pow(
+        Math.min(fuelUsed, maxFuelPerJump) / fuelMul,
+        1 / fuelPower,
+    ) * optMass) / mass;
+}
+
 /**
  * Get the jump range metrics of a ship.
  * @param ship Ship
@@ -38,27 +56,24 @@ export function getJumpRangeMetrics(
 ): IJumpRangeMetrics {
     const jumpBoost = getJumpBoost(ship, modified);
     const fsd = ship.getFSD();
-    const optMass = fsd.getClean('fsdoptimalmass', modified);
     let mass = getLadenMass(ship, modified);
 
     const maxFuelPerJump = fsd.getClean('maxfuel', modified);
-    const fuelMul = fsd.getClean('fuelmul', modified);
-    const fuelPower = fsd.getClean('fuelpower', modified);
     let fuel = getFuel(ship, modified);
 
     let jumpRange = 0;
     let totalRange = 0;
     // If there is no fuel, loopCount will be zero so jumpRange will as well
     const loopCount = Math.ceil(fuel / maxFuelPerJump);
-    let fuelPerJump = 0;
+    let fuelUsed = 0;
     for (let i = 0; i < loopCount; i++) {
         // decrease mass and fuel by fuel from last jump
-        mass -= fuelPerJump;
-        fuel -= fuelPerJump;
-        fuelPerJump = Math.min(fuel, maxFuelPerJump);
-        const thisJump =
-            (Math.pow(fuelPerJump / fuelMul, 1 / fuelPower) * optMass) / mass +
-            jumpBoost;
+        mass -= fuelUsed;
+        fuel -= fuelUsed;
+        fuelUsed = Math.min(fuel, maxFuelPerJump);
+        const thisJump = calculateJumpRange(
+            ship, mass, fuelUsed, modified,
+        ) + jumpRange;
         if (i === 0) {
             jumpRange = thisJump;
         }
